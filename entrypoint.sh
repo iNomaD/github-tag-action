8e2bfca7a7f8e28caf3ad65120341be5147b27bf
Denis Korobov <dkorobov@devexperts.com>
2019-12-20 13:23:02 +0300

    Support for version file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,85 +1,55 @@
 #!/bin/bash
 
 # config
-default_tag=${DEFAULT_TAG:-0.0.0}
 with_v=${WITH_V:-false}
-release_branches=${RELEASE_BRANCHES:-master}
-custom_tag=${CUSTOM_TAG}
+version_file_path=${VERSION_FILE_PATH:-version}
 
-pre_release="true"
-IFS=',' read -ra branch <<< "$release_branches"
-for b in "${branch[@]}"; do
-    echo "Is $b a match for ${GITHUB_REF#'refs/heads/'}"
-    if [[ "${GITHUB_REF#'refs/heads/'}" =~ $b ]]
-    then
-        pre_release="false"
-    fi
-done
-echo "pre_release = $pre_release"
+# get current version
+function prop {
+    grep "${1}" "$version_file_path"|cut -d'=' -f2
+}
+major=$(prop 'MAJOR')
+minor=$(prop 'MINOR')
+patch=$(prop 'PATCH')
+prerel=$(prop 'PREREL')
+version="$major.$minor.$patch-$prerel"
+compare=$(semver compare "$version" "$version")
+if [ ! "$compare" == "0" ]; then
+    echo "Wrong version $version. Skipping..."
+    exit 0
+fi
 
 # fetch tags
 git fetch --tags
 
 # get latest tag
-tag_pattern='*.*.*'
-tag=$(git describe --tags `git rev-list --tags=$tag_pattern --max-count=1`)
-tag_commit=$(git rev-list -n 1 $tag)
-echo "Latest tag: $tag"
+tag_pattern="$version*"
+tag_commit=$(git rev-list --tags="$tag_pattern" --max-count=1)
+echo "Latest tagged commit: $tag_commit"
+if [ -n "$tag_commit" ]; then
+    tag=$(git describe --tags "$tag_commit")
+    echo "Latest tag: $tag"
+fi
 
 # get current commit hash for tag
 commit=$(git rev-parse HEAD)
 
 if [ "$tag_commit" == "$commit" ]; then
     echo "No new commits since previous tag. Skipping..."
-    echo ::set-output name=tag::$tag
+    echo ::set-output name=tag::"$tag"
     exit 0
 fi
 
-# if there are none, start tags at default_tag
+# if there are none, start tags at specified version
 if [ -z "$tag" ]
 then
-    tag=$default_tag
-    new=$(semver bump prerel SNAPSHOT $tag)
-    log=''
+    new=$version
 else
-    new=$tag
-    log=$(git log $tag_commit..HEAD --format='%H')
-fi
-
-for i in $log; do
-    # get commit message
-    message=$(git log --pretty=oneline $i | grep $i)
-    echo "New commit: $message"
-
-    # get commit logs and determine home to bump the version
-    # supports #major, #minor, #patch, #release
-    case "$message" in
-        *#major* )
-          new=$(semver bump major $new)
-          new=$(semver bump prerel SNAPSHOT $new)
-          ;;
-        *#minor* )
-          new=$(semver bump minor $new)
-          new=$(semver bump prerel SNAPSHOT $new)
-          ;;
-        *#patch* )
-          new=$(semver bump patch $new)
-          new=$(semver bump prerel SNAPSHOT $new)
-          ;;
-        *#release* )
-          new=$(semver bump release $new)
-          ;;
-    esac
-done
-
-# if version was not bumped then increment build number
-if [ "$new" == "$tag" ]; then
     # get and increment build number
     # will default to 1 if it was not a number
-    build=$(semver get build $tag)
+    build=$(semver get build "$tag")
     build=$((build+1))
-    new=$(semver bump prerel SNAPSHOT $new)
-    new=$(semver bump build `echo $build` $new)
+    new=$(semver bump build $build "$tag")
 fi
 
 # prefix with 'v'
@@ -88,36 +58,20 @@ then
     new="v$new"
 fi
 
-if $pre_release
-then
-    new="$new-${commit:0:7}"
-fi
-
-if [ ! -z $custom_tag ]
-then
-    new="$custom_tag"
-fi
-
-echo $new
+echo "$new"
 
 # set outputs
-echo ::set-output name=new_tag::$new
-echo ::set-output name=tag::$new
-
-if $pre_release
-then
-    echo "This branch is not a release branch. Skipping the tag creation."
-    exit 0
-fi
+echo ::set-output name=new_tag::"$new"
+echo ::set-output name=tag::"$new"
 
 # push new tag ref to github
 dt=$(date '+%Y-%m-%dT%H:%M:%SZ')
 full_name=$GITHUB_REPOSITORY
-git_refs_url=$(jq .repository.git_refs_url $GITHUB_EVENT_PATH | tr -d '"' | sed 's/{\/sha}//g')
+git_refs_url=$(jq .repository.git_refs_url "$GITHUB_EVENT_PATH" | tr -d '"' | sed 's/{\/sha}//g')
 
 echo "$dt: **pushing tag $new to repo $full_name"
 
-curl -s -X POST $git_refs_url \
+curl -s -X POST "$git_refs_url" \
 -H "Authorization: token $GITHUB_TOKEN" \
 -d @- << EOF
 
